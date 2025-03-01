88787fc0ae9f8634215ec0925f937c2a4068afa1
ARNOLD <146892654+OMGSAM@users.noreply.github.com>
2025-03-01 14:44:05 +0000

    Create tinker_test.php

---
+++ tinker_test.php
@@ -0,0 +1,5 @@
+php artisan tinker
+
+
+Product::create(['name' => 'Produit A', 'description' => 'Description du produit A', 'price' => 100]);
+Product::create(['name' => 'Produit B', 'description' => 'Description du produit B', 'price' => 150]);
