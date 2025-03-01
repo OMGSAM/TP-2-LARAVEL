48e4b3e7d6751fe0bc4faa1f29af99669af4b985
ARNOLD <146892654+OMGSAM@users.noreply.github.com>
2025-03-01 14:43:28 +0000

    Create route_web.php

---
+++ route_web.php
@@ -0,0 +1,5 @@
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
